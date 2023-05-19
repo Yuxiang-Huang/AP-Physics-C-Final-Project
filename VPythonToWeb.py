f38cc753b4997721464785dc40e7f7918096c63c
Yuxiang Huang <yhuang30@stuy.edu>
2023-05-18 22:07:55 -0400

    write a program to convert from vpython to web

---
+++ VPythonToWeb.py
@@ -0,0 +1,6 @@
+file = open("finalProject.py").read()
+file = file.replace("import vpython", "")
+file = file.replace("vpython.", "")
+file = file.replace("None", "undefined")
+web = open("web.txt", 'w')
+web.write('Web VPython 3.2' + file)
